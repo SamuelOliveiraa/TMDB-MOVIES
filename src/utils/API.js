1a1e70493e2bc48025490a8953f4f18148fca5f2
Samuel Oliveira <oliveira_samuel14@hotmail.com>
2024-05-31 13:06:06 -0300

    Adicionando imagens e .env

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,18 +1,20 @@
+const apiKey = import.meta.env.VITE_API_KEY;
+const apiURL = import.meta.env.VITE_API_URL;
 class API {
   static async get(page) {
-    const data = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=acef416ba094cca22bc2cb7888b92b61&page=${page}`);
+    const data = await fetch(`${apiURL}discover/movie?api_key=${apiKey}&page=${page}`);
     const res = await data.json();
     return res.results;
   }
 
   static async getById(id) {
-    const data = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=acef416ba094cca22bc2cb7888b92b61`);
+    const data = await fetch(`${apiURL}movie/${id}?api_key=${apiKey}`);
     const res = await data.json();
     return res;
   }
 
   static async search(query, page) {
-    const data = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=acef416ba094cca22bc2cb7888b92b61&query=${query}&page=${page}`);
+    const data = await fetch(`${apiURL}search/movie?api_key=${apiKey}&query=${query}&page=${page}`);
     const res = await data.json();
     return res;
   }
